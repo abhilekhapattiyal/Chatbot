95d3a650ebdbf180c84a0c27f3b7d40460903446
abhilekhapattiyal <132445361+abhilekhapattiyal@users.noreply.github.com>
2024-11-17 12:41:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -12,12 +12,22 @@ genai.configure(
 model=genai.GenerativeModel('gemini-pro')
 chat=model.start_chat(history=[])
 
-while(True):
-    question=input("You:")
-    if(question.strip()==''):
-        break
-    response=chat.send_message(question)
-    print('\n')
-    print(f"Bot: {response.text}")
-    print("\n")
-    
+def get_gemini_response(question):
+    response=chat.send_message(question,stream=True)
+    return response
+st.set_page_config(page_title="AI Chatbot")
+st.header("Gemini AI Chatbot")
+if 'chat_history' not in st.session_state:
+    st.session_state['chat_history']=[]
+input=st.text_input("Input:",key="input")
+submit=st.button("Ask the question")
+if submit and input:
+    response=get_gemini_response(input)
+    st.session_state['chat_history'].append(("You:",input))
+    st.subheader("The Response is")
+    for chunk in response:
+        st.write(chunk.text)
+        st.session_state['chat_history'].append(("Bot:",chunk.text))
+st.subheader("The chat history is")
+for role,text in st.session_state['chat_history']:
+    st.write(f"{role}:{text}")  
